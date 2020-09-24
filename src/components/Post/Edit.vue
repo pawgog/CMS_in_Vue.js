8bf1682e5116b03fbdd4db65c6bfb89e94ef7ce7
pawgog <p.gogola@interia.pl>
2020-09-24 14:01:35 +0200

    Add edit component.

---
+++ src/components/Post/Edit.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-card class="card-content" no-body>
+    <div class="card-content">
+      <h3>Edit post</h3>
+    </div>
+    <div class="card-body">
+      <b-form @submit="onSubmit">
+        <b-form-group id="input-group-1" label="Title:" label-for="title">
+          <b-form-input id="title" v-model="form.title" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Date:" label-for="date">
+          <b-form-input id="date" v-model="form.date" type="date" required></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-3" label="Content" label-for="content">
+          <b-form-textarea id="content" v-model="text" rows="3" max-rows="6"></b-form-textarea>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "PostEdit",
+  props: ["slug"],
+  data() {
+    return {
+      post: {},
+      error: false,
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {
+    this.$http
+      .get(`/post/${this.slug}`)
+      .then(({ data }) => {
+        const [getData] = data;
+        this.post = getData;
+      })
+      .catch(() => {
+        this.$router.push({ name: "404" });
+      });
+  },
+};
+</script>
