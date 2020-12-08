d77478c0b421302f0ab2f3ff4eb2eb4b4348bbe9
pawgog <p.gogola@interia.pl>
2020-12-09 00:15:13 +0100

    Router back to home page.

--- src/components/General/404.vue
+++ src/components/General/404.vue
@@ -3,6 +3,7 @@
     <div class="text-error">
       <h3><b-icon icon="exclamation-circle"></b-icon>404</h3>
       <h4>Page not found!</h4>
+      <router-link :to="{ name: 'home' }" class="btn-back">Back</router-link>
     </div>
   </b-card>
 </template>
@@ -18,8 +19,18 @@ export default {
     margin: 0 auto;
     text-align: center;
     color: red;
+    padding: 25px 0 50px 0;
     & .b-icon {
       margin-right: 10px;
+    }
+    & .btn-back {
+      position: relative;
+      top: 15px;
+      padding: 10px 20px;
+      border-radius: 5px;
+      color: white;
+      background-color: red;
+      text-decoration: none;
     }
   }
 </style>
