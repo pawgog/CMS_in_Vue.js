6a7fc6fa8cd1691f91b004d233679456259c8bff
pawgog <pgogola10@gmail.com>
2020-12-30 18:21:56 +0100

    Edit form items.

--- src/components/Post/Add.vue
+++ src/components/Post/Add.vue
@@ -8,11 +8,17 @@
         <b-form-group id="input-group-1" label="Title:" label-for="title">
           <b-form-input id="title" v-model="post.title" type="text" required></b-form-input>
         </b-form-group>
-        <b-form-group id="input-group-2" label="Date:" label-for="date_posted">
+        <b-form-group id="input-group-2" label="Author:" label-for="author">
+          <b-form-input id="author" v-model="post.author" type="text" required></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-3" label="Date:" label-for="date_posted">
           <b-form-input id="date_posted" v-model="post.date_posted" type="date" required></b-form-input>
         </b-form-group>
-        <b-form-group id="input-group-3" label="Content" label-for="content">
+        <b-form-group id="input-group-4" label="Content" label-for="content">
           <b-form-textarea id="content" v-model="post.content" rows="3" max-rows="6"></b-form-textarea>
+        </b-form-group>
+        <b-form-group id="input-group-5" label="Image url:" label-for="img">
+          <b-form-textarea id="img" v-model="post.img" type="text"></b-form-textarea>
         </b-form-group>
         <b-button variant="danger" @click="$router.go(-1)">Back</b-button>
         <b-button type="submit" variant="primary" :disabled="!filled">Submit</b-button>
@@ -32,8 +38,10 @@ export default {
       error: false,
       form: {
         title: "",
+        author: "",
         date_posted: "",
         content: "",
+        img: "",
       },
     };
   },
@@ -45,12 +53,14 @@ export default {
   },
   methods: {
     onSave() {
-      const { title, date_posted, content} = this.post;
+      const { title, author, date_posted, content, img } = this.post;
       this.$http
-        .post(`${server.serverURL}/posts`, {
+        .post(`${server.serverURL}/post`, {
           title,
+          author,
           date_posted,
-          content
+          content,
+          img,
         })
         .then(({ data }) => {
           this.$router.push({ name: "post", params: { slug: data.slug } });
--- src/components/Post/Edit.vue
+++ src/components/Post/Edit.vue
@@ -8,11 +8,17 @@
         <b-form-group id="input-group-1" label="Title:" label-for="title">
           <b-form-input id="title" v-model="post.title" type="text" required></b-form-input>
         </b-form-group>
-        <b-form-group id="input-group-2" label="Date:" label-for="date_posted">
+        <b-form-group id="input-group-2" label="Author:" label-for="author">
+          <b-form-input id="author" v-model="post.author" type="text" required></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-3" label="Date:" label-for="date_posted">
           <b-form-input id="date_posted" v-model="post.date_posted" type="date" required></b-form-input>
         </b-form-group>
-        <b-form-group id="input-group-3" label="Content" label-for="content">
+        <b-form-group id="input-group-4" label="Content" label-for="content">
           <b-form-textarea id="content" v-model="post.content" rows="3" max-rows="6"></b-form-textarea>
+        </b-form-group>
+        <b-form-group id="input-group-5" label="Image url:" label-for="img">
+          <b-form-textarea id="img" v-model="post.img" type="text"></b-form-textarea>
         </b-form-group>
         <b-button variant="danger" @click="$router.go(-1)">Back</b-button>
         <b-button type="submit" variant="primary" :disabled="!filled">Submit</b-button>
@@ -33,8 +39,10 @@ export default {
       error: false,
       form: {
         title: "",
+        author: "",
         date_posted: "",
         content: "",
+        img: "",
       },
     };
   },
@@ -46,12 +54,14 @@ export default {
   },
   methods: {
     onSubmit() {
-      const { _id, title, date_posted, content} = this.post;
+      const { _id, title, author, date_posted, content, img } = this.post;
       this.$http.put(`${server.serverURL}/post/${_id}`, {
         _id,
         title,
+        author,
         date_posted,
         content,
+        img,
       })
       .catch(() => {
         this.error = true;
